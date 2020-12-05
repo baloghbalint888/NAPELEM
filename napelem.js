75c3de55f9ad75cc70e899ff540c1dcc08457c18
Balogh Bálint <baloghbalint888@gmail.com>
2020-12-05 12:15:51 +0100

    napelem.js valtoztatas

--- napelem.js
+++ napelem.js
@@ -2,7 +2,7 @@ function panelek(db){
     const panelek=document.getElementById('panelek');
     panelek.innerHTML="";
     for (i = 0; i < db; i++) {
-        panelek.innerHTML += "<img src='napelem_ikon.png'>"; 
+        panelek.innerHTML += "<img src='kepek/napelem_ikon.png'>"; 
     }
     panelek.innerHTML += "("+db+" db)";
     document.getElementById('osszteljesitmeny').value = db * 275;
